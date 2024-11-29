f852652928416971a615e493cd042689380527d8
Ayan <nazbievayan16@gmail.com>
2024-11-29 14:15:13 +0500

    task 7 - DONE

--- 30 days of javascript/task7.js
+++ 30 days of javascript/task7.js
@@ -1,11 +1,17 @@
 var once = function(fn) {
-
+    let executed = false;
     return function(...args){
-        fn(...args)
+        if (!executed){
+            executed = true;
+            return fn(...args)
+        } else {
+            return undefined
+        }
     }
 };
 
-let fn = (a,b,c) => (a + b + c)
+let fn = (a,b,c) => (a * b * c)
 let onceFn = once(fn)
 
-console.log(onceFn(1,2,3))
+console.log(onceFn(5,7,4))
+console.log(onceFn(5,7,4))
